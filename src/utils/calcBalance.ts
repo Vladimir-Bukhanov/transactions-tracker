780974623ebf9ac00cdea72513b84cbfe513cb61
Vladimir Bukhanov <vladimirbukhanoff@gmail.com>
2026-03-11 21:18:18 +0300

    check filter

--- src/utils/calcBalance.ts
+++ src/utils/calcBalance.ts
@@ -2,12 +2,10 @@ import type { Transaction } from '../types/transaction'
 
 export const calcBalance = (transactions: Transaction[]) => {
 
-	const totalIncome = transactions
-	.filter(t => t.type === "income")
+	const totalIncome = transactions?.filter(t => t.type === "income")
 	.reduce((acc, c) => acc + Number(c.amount), 0)
 
-	const totalExpenses = transactions
-		.filter(t => t.type === "expense")
+	const totalExpenses = transactions?.filter(t => t.type === "expense")
 		.reduce((acc, c) => acc + Number(c.amount), 0)
 
 	const balance = totalIncome - totalExpenses
